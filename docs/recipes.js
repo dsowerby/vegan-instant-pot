5fa545e353bc35733a3fa6edb0498399f3103002
Dave Sowerby-Williams <dave.sowerby@gmail.com>
2021-05-04 08:34:21 +0100

    Initial commit

---
+++ docs/recipes.js
@@ -0,0 +1,5 @@
+var veganInstantPot = [
+    { location: 'The Vegan Instant Pot p. 44', name: 'Mushroom Broth', type: 'soup'},
+    { location: 'The Vegan Instant Pot p. 47', name: 'Vegetable Broth', type: 'soup'},
+    { location: 'The Vegan Instant Pot p. 49', name: 'Vegan Cheese Sauec + Vegan Queso', type: 'soup'},
+];
