696606fc09b04d599ec026121d9188d19de4f655
Dave Sowerby-Williams <dave.sowerby@gmail.com>
2021-05-04 09:19:33 +0100

    Next few recipes

--- docs/recipes.js
+++ docs/recipes.js
@@ -1,5 +1,11 @@
 var veganInstantPot = [
     { location: 'The Vegan Instant Pot p. 44', name: 'Mushroom Broth', type: 'soup'},
     { location: 'The Vegan Instant Pot p. 47', name: 'Vegetable Broth', type: 'soup'},
-    { location: 'The Vegan Instant Pot p. 49', name: 'Vegan Cheese Sauec + Vegan Queso', type: 'soup'},
+    { location: 'The Vegan Instant Pot p. 49', name: 'Vegan Cheese Sauce + Vegan Queso', type: ''},
+    { location: 'The Vegan Instant Pot p. 53', name: 'Restaurant-Style Hummus + Three Variations', type: 'humous humus dip'},
+    { location: 'The Vegan Instant Pot p. 56', name: 'Game Day Black Bean Dip', type: ''},
+    { location: 'The Vegan Instant Pot p. 59', name: 'Fiery Arrabiata Sauce', type: ''},
+    { location: 'The Vegan Instant Pot p. 61', name: 'Caramelized Onion Jam', type: 'caramelised'},
+    { location: 'The Vegan Instant Pot p. 65', name: 'Blueberry Mojiti Jam', type: ''},
+
 ];
